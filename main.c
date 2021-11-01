40a8038336fe4acefe1599c0e2f7ea4d2ed76500
Petr Velička <petrvelicka@tuta.io>
2021-11-01 14:39:59 +0100

    greeter

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello World!\n");
+greet(int num) {
+    printf("hello %d\n", num);
+    return num + 1;
+}
+
+main() {
+    int number = greet(10);
+    printf("%d\n", number);
     return 0;
 }
